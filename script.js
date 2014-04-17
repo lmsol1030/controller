dc7eda8ce077adc55409126cd55038992dae6f8e
lmsol1030 <lmsol1030@naver.com>
2014-04-17 12:41:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-$(document).ready(function() {
-    $(document).keydown(function(key) {
-        switch(parseInt(key.which,10)) {
-			// Left arrow key pressed
-			case 37://s
-				$('img').animate({left: "-=10px"}, 'fast');
-				break;
-			// Up Arrow Pressed
-			case 38://e
-				$('img').animate({Up: "+=10px"}, 'fast');
-				break;
-			// Right Arrow Pressed
-			case 39//f
-				$('img').animate({Right: "-=10px"}, 'fast');
-				break;
-			// Down Array Pressed
-			case 40://c
-				$('img').animate({Down: "+=10px"}, 'fast');
-				break;
-		}
-	});
+$(document).ready(function(){
+    $('in-btn').fadeOut('.fast');
 });
