055ef9de23937f8db0f56e06938114878f8649f5
lmsol1030 <lmsol1030@naver.com>
2014-04-18 12:42:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    $('#up-btn').click(function(){
+    $('#player').animate({top:"+=100px"},1000);
+    });
     $('#in-btn').click(function(){
     $('#player').fadeOut('fast');
     });
