f6f222f1fef80a2ca535baba4f242a17b1a06bfd
lmsol1030 <lmsol1030@naver.com>
2014-04-18 12:46:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ $(document).ready(function(){
    
       
         $('#left-btn').click(function(){
-    $('#player').animate({top:"-=100px"},1000);
+    $('#player').animate({left:"-=100px"},1000);
     });
     
     
         $('#right-btn').click(function(){
-    $('#player').animate({top:"+=100px"},1000);
+    $('#player').animate({left:"+=100px"},1000);
     });
    
    
